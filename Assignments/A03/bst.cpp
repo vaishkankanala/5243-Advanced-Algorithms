731a725020515aa07339d69fa9c2f6a48d0d6851
Vaishnavi Chowdary Kankanala <vaishu@Vaishnavis-MacBook-Air.local>
2025-02-04 11:19:36 -0600

    Added A03 assignment

---
+++ Assignments/A03/bst.cpp
@@ -0,0 +1,180 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = nullptr;
+    }
+};
+
+class GraphvizBST {
+public:
+    static void saveDotFile(const std::string &filename, const std::string &dotContent) {
+        std::ofstream outFile(filename);
+        if (outFile.is_open()) {
+            outFile << dotContent;
+            outFile.close();
+            std::cout << "DOT file saved: " << filename << std::endl;
+        } else {
+            std::cerr << "Error: Could not open file " << filename << std::endl;
+        }
+    }
+
+    static std::string generateDot(const Node *root) {
+        std::string dot = "digraph BST {\n";
+        dot += "    node [fontname=\"Arial\"];\n";
+        dot += generateDotHelper(root);
+        dot += "}\n";
+        return dot;
+    }
+
+private:
+    static std::string generateDotHelper(const Node *node) {
+        if (!node)
+            return "";
+        std::string result;
+        if (node->left) {
+            result += "    " + std::to_string(node->data) + " -> " + std::to_string(node->left->data) + " [label=\"L\"];\n";
+            result += generateDotHelper(node->left);
+        } else {
+            std::string nullNode = "nullL" + std::to_string(node->data);
+            result += "    " + nullNode + " [shape=point];\n";
+            result += "    " + std::to_string(node->data) + " -> " + nullNode + ";\n";
+        }
+        if (node->right) {
+            result += "    " + std::to_string(node->data) + " -> " + std::to_string(node->right->data) + " [label=\"R\"];\n";
+            result += generateDotHelper(node->right);
+        } else {
+            std::string nullNode = "nullR" + std::to_string(node->data);
+            result += "    " + nullNode + " [shape=point];\n";
+            result += "    " + std::to_string(node->data) + " -> " + nullNode + ";\n";
+        }
+        return result;
+    }
+};
+
+class Bst {
+    Node *root;
+
+    void _print(Node *subroot) {
+        if (!subroot) {
+            return;
+        } else {
+            _print(subroot->left);
+            cout << subroot->data << " ";
+            _print(subroot->right);
+        }
+    }
+    void _insert(Node *&subroot, int x) {
+        if (!subroot) { // if(root == nullptr)
+            subroot = new Node(x);
+        } else {
+            if (x < subroot->data) {
+                _insert(subroot->left, x);
+            } else {
+                _insert(subroot->right, x);
+            }
+        }
+    }
+    void _delete(Node *&subroot, int value) {
+        // no value in tree
+        if (!subroot) {
+            return;
+        }
+        //value is less than root
+        if (value < subroot->data){
+            _delete(subroot->left, value);
+        }
+        //value is greater than root
+        else if (value > subroot->data){
+            _delete(subroot->right, value);
+        }
+        //value is found 
+        else{
+            //no left child
+            if(subroot->left == nullptr){
+                Node *temp = subroot->right;
+                delete subroot;
+                subroot = temp;
+            }
+            //no right child
+            else if(subroot->right == nullptr){
+                Node *temp = subroot->left;
+                delete subroot;
+                subroot = temp;
+            }
+            else{
+                Node *temp = minvalue(subroot->right);
+                subroot->data = temp->data;
+                _delete(subroot->right, temp->data);
+            }
+        }
+    }
+    //to find inorder succesor 
+    Node* minvalue(Node* subroot){
+        Node* temp = subroot;
+        if(temp->left != nullptr){
+            temp = temp->left;
+        }
+        return temp;
+    }
+
+public:
+    Bst() { root = nullptr; }
+    void insert(int x) { _insert(root, x); }
+    bool search(int key) { return 0; }
+    void print() { _print(root); }
+    void saveDotFile(const std::string &filename) {
+        std::string dotContent = GraphvizBST::generateDot(root);
+        GraphvizBST::saveDotFile(filename, dotContent);
+    }
+    void deleteNode(int value) {
+        _delete(root, value);
+    }
+};
+
+bool unique_value(int *arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    Bst tree;
+    int root = pow(2, 15) / 2;
+    int max = pow(2, 15) - 1;
+    vector<int> arr;
+    arr.push_back(root);
+    tree.insert(root);
+    for (int i = 1; i < 5000; i++) {
+        int r = rand() % max;
+        while (!unique_value(arr.data(), arr.size(), r)) {
+            r = rand() % max;
+        }
+        tree.insert(r);
+        arr.push_back(r);
+    }
+
+    cout << "BST Inorder Traversal: ";
+    tree.print();
+    tree.saveDotFile("bst_snapshot.dot");
+    
+    tree.deleteNode(30);
+    cout << endl << "After deleting: " << endl;
+    tree.print();
+    tree.saveDotFile("bst_snapshot.dot");
+
+    return 0;
+}
